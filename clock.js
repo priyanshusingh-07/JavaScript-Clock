2427718f0ed97c26f14f820ca23d9e755f173684
priyanshusingh-07 <priyanshu7606@gmail.com>
2024-12-24 11:36:09 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,22 @@
+function timeClock(){
+    let a = new Date();
+    let h=a.getHours();
+    let m=a.getMinutes();
+    let s=a.getSeconds();
+    document.getElementById('hour').innerHTML=h+':';
+    document.getElementById('min').innerHTML=m+':';;
+    document.getElementById('sec').innerHTML=s+'- ';
+    if(h>12){
+        ses.innerText=' PM'
+    }else{
+        ses.innerText= ' AM'
+    }
+    if(h>12){
+        h=-12;
+    }
+}
+
+let hour=document.getElementById('hour');
+let ses =document.getElementById('ses');
+
+setInterval(timeClock,10);
